190373c78d3180b58b73dbfa26ade4bde22e192c
Mustafa-Dara-Ozevin <mustafadara.ozevin@outlook.com>
2023-04-22 00:03:28 +0200

    solve sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(start, finish) {
+    let sum = 0;
 
+    if (!Number.isInteger(start) || !Number.isInteger(finish)){
+        return "ERROR";
+    }
+    if (start < 0 || finish < 0){
+        return "ERROR";
+    }
+    if (start > finish){
+        let temp = start;
+        start = finish;
+        finish = temp;
+    }
+    for (let i=start;i<=finish;i++){
+        sum+=i;
+    }
+    return sum
 };
 
 // Do not edit below this line

81e13baa85e84fbe098b74a08a34894baf4e9f23
Mustafa-Dara-Ozevin <mustafadara.ozevin@outlook.com>
2023-04-21 23:19:54 +0200

    solve repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
-
+const repeatString = function(word,times) {
+    if (times < 0){
+        return "ERROR";
+    }else if(times === 0){
+        return "";
+    }else{
+        if (word === ""){
+            return "";
+        }else{
+            let repeated = "";
+            for(let i=0; i<times;i++){
+                repeated += word;
+            }
+            return repeated
+        }
+    }
 };
 
 // Do not edit below this line

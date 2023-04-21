5c3e87b5e3f4c897374e456de1752e232aaa3cde
Mustafa-Dara-Ozevin <mustafadara.ozevin@outlook.com>
2023-04-21 23:25:37 +0200

    solve reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let reversed = "";
+    const letters = word.split("");
+    for (let i=letters.length-1;i>=0;i--){
+        reversed += letters[i];
+    }
+    return reversed;
 };
 
 // Do not edit below this line

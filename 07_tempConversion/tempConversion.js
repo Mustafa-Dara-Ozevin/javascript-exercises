87f47d7e59a9a024858385c7d4a4db9eea4957ce
Mustafa-Dara-Ozevin <mustafadara.ozevin@outlook.com>
2023-04-22 10:03:19 +0200

    solve tempConversion.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let fahrTemp = (temp-32)/1.8;
+  return Math.round(fahrTemp*10)/10;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let celsTemp = temp*1.8 + 32;
+  return Math.round(celsTemp*10)/10;
 };
 
 // Do not edit below this line

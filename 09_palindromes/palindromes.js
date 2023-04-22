fa22508be3d8ced7325bd0bf3b8e7cdda393feee
Mustafa-Dara-Ozevin <mustafadara.ozevin@outlook.com>
2023-04-22 12:59:28 +0200

    solve palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replace(/ /g,'').toLowerCase();
+    let arr = str.split('');
+    let rev_arr = arr.map((x)=> x);
+    rev_arr.reverse();
 
+    for (let i= 0;i<arr.length;i++){
+        if (arr[i] !== rev_arr[i]){
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line

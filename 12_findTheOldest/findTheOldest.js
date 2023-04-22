3fd018f95504225f599c8d98a26f7710e75e84cf
Mustafa-Dara-Ozevin <mustafadara.ozevin@outlook.com>
2023-04-22 13:31:10 +0200

    solve findTheOldest.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let ages = [];
+    for (person of people){
+        if (Object.hasOwn(person,"yearOfDeath")){
+            ages.push(person.yearOfDeath-person.yearOfBirth);
+        }else{
+            ages.push(new Date().getFullYear()-person.yearOfBirth)
+        }
+    }
+    let i = ages.indexOf(Math.max(...ages));
+    return people[i];
 
 };
 

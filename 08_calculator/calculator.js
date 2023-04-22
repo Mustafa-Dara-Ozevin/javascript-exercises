e58c5148446a3645690bfb0ff734473c8b0d1c71
Mustafa-Dara-Ozevin <mustafadara.ozevin@outlook.com>
2023-04-22 12:33:43 +0200

    solve calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,54 @@
-const add = function() {
+const add = function(num1,num2) {
+  return num1 + num2;
 	
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  if (nums.length ===0){
+    return 0;
+  }
+	let sum = 0;
+    for (num of nums){
+      sum += num;
+    }
+    return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  if (nums.length === 0){
+    return 0;
+  }
+  let result = nums[0];
+  for (let i=1; i<nums.length;i++){
+    result *= nums[i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+	if (power === 0){
+      return 1;
+    }
+    let result = num;
+    for (let i=0; i<power-1;i++){
+      result *= num;
+    }
+    return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0){
+    return 1;
+  }
+	let result = 1;
+    for (let i=2;i<=num;i++){
+      result *= i;
+    }
+    return result;
 };
 
 // Do not edit below this line

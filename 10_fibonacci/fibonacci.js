704cf7d342015df0d57b3085c8f135b306759330
Mustafa-Dara-Ozevin <mustafadara.ozevin@outlook.com>
2023-04-22 13:12:42 +0200

    solve fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (!Number.isInteger(num)){
+        num = parseInt(num);
+    }
+    if (num < 0){
+        return "OOPS"
+    }
+    let current = 1;
+    let previous1 = 1;
+    let previous2 = 0;
+    for (let i = 1; i<num; i++){
+        current = previous1 + previous2;
+        previous2 = previous1;
+        previous1 = current;
+    }
+    return current
 };
 
 // Do not edit below this line
